d3e1a06f32ba59ff98b73238dff345308b851155
Daniel M <danielmiao2019@icloud.com>
2021-10-27 14:12:49 -0700

    added start button clicked events

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtTest import *
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 
@@ -9,10 +10,12 @@ class Text(QLabel):
 	"""Override existing QLabel for color animation"""
 	def __init__(self, parent, text=""):
 		super().__init__(parent=parent)  # Initialize text
+		self.text_color = None
 		self.setText(text)  # Set text content
 
 	def setColor(self, color):
 		"""Color animation"""
+		self.text_color = color  # Update color variable
 		palette = self.palette()  # Create new palette
 		palette.setColor(self.foregroundRole(), color)  # Set color for palette
 		self.setPalette(palette)  # Set color of text to palette color
@@ -22,9 +25,11 @@ class Text(QLabel):
 
 class Button(QPushButton):
 	"""Override existsing QPushButton for custom hover events"""
-	def __init__(self, parent, text=""):
+	def __init__(self, parent, text="", mouse_press_event=None):
 		super().__init__(parent=parent)  # Initialize button
 		self.letter_spacing = "2px"  # Set letter spacing variable
+		self.background_color = None
+		self.mouse_press_function = mouse_press_event
 		self.setText(text)  # Set text content
 		self.setFont(QFont("Impact", 15))  # Set text font
 		self.setCursor(Qt.PointingHandCursor)
@@ -42,6 +47,8 @@ class Button(QPushButton):
 
 	def mousePressEvent(self, event):
 		"""Mouse press event"""
+		if self.mouse_press_function is not None:
+			self.mouse_press_function()
 		super().mousePressEvent(event)
 
 	def mouseReleaseEvent(self, event):
@@ -50,9 +57,18 @@ class Button(QPushButton):
 
 	def setBackgroundColor(self, color: QColor):
 		"""Background color animation"""
-		self.setStyleSheet(f"background-color: rgba{color.getRgb()}; border: none; letter-spacing: {self.letter_spacing};")
+		self.background_color = color  # Update background color variable
+		self.setStyleSheet(f"background-color: rgba{color.getRgb()}; border: none; letter-spacing: {self.letter_spacing};")  # Update stylesheet
+
+	def setColor(self, color):
+		"""Color animation"""
+		self.text_color = color  # Update color variable
+		palette = self.palette()  # Create new palette
+		palette.setColor(self.foregroundRole(), color)  # Set color for palette
+		self.setPalette(palette)  # Set color of text to palette color
 
 	background = pyqtProperty(QColor, fset=setBackgroundColor)
+	color = pyqtProperty(QColor, fset=setColor)
 
 
 class GameWindow(QWidget):
@@ -75,7 +91,7 @@ class MainWindow(QWidget):
 		self.title_animation.finished.connect(lambda: self.changeAnimationDirection(self.title_animation))  # Call lambda: sel... when animation finishes
 		self.title_animation.start()  # Start animation
 		# Start button
-		self.start_button = Button(self, "Start Game")
+		self.start_button = Button(self, "Start Game", mouse_press_event=self.start)
 		# Start button animation
 		self.button_animation = QPropertyAnimation(self.start_button, b"background")  # Create animation
 		self.button_animation.setLoopCount(1)  # Set loop count
@@ -85,7 +101,44 @@ class MainWindow(QWidget):
 		self.button_animation.finished.connect(lambda: self.changeAnimationDirection(self.button_animation))  # Call function when animation finishes
 		self.button_animation.start()  # Start animation
 
+	def start(self):
+		"""First phase of starting game"""
+		# Fade objects
+		# Title animation
+		self.title_animation.stop()  # Stop existing title animation
+		# Create new title text color animation
+		self.title_animation = QPropertyAnimation(self.title, b"color")  # Create animation
+		self.title_animation.setLoopCount(1)  # Set loop count
+		self.title_animation.setDuration(500)  # Set loop duration
+		self.title_animation.setStartValue(self.title.text_color)  # Set start color
+		self.title_animation.setEndValue(QColor.fromRgb(*self.title.text_color.getRgb()[:-1], 0))  # Set end color
+		self.title_animation.finished.connect(self.start1)  # Call self.start1 when animation finishes
+		self.title_animation.start()  # Start animation
+		# Start button animation
+		self.button_animation.stop()  # Stop existing start button animation
+		self.start_button.mouse_press_function = None
+		# Create new start button background color animation
+		self.button_animation = QPropertyAnimation(self.start_button, b"background")  # Create animation
+		self.button_animation.setLoopCount(1)  # Set loop count
+		self.button_animation.setDuration(500)  # Set loop duration
+		self.button_animation.setStartValue(self.start_button.background_color)  # Set start color
+		self.button_animation.setEndValue(QColor.fromRgb(*self.start_button.background_color.getRgb()[:-1], 0))  # Set end color
+		self.button_animation.finished.connect(lambda: self.start_button.setCursor(Qt.ArrowCursor))
+		self.button_animation.start()  # Start animation
+		# Create new start button text color animation
+		self.button_animation1 = QPropertyAnimation(self.start_button, b"color")  # Create animation
+		self.button_animation1.setLoopCount(1)  # Set loop count
+		self.button_animation1.setDuration(500)  # Set loop duration
+		self.button_animation1.setStartValue(QColor("#000000"))  # Set start color
+		self.button_animation1.setEndValue(QColor("transparent"))  # Set end color
+		self.button_animation1.start()  # Start animation
+
+	def start1(self):
+		"""Second phase of starting game"""
+		print("Start")
+
 	def changeAnimationDirection(self, animation):
+		"""Invert animation direction"""
 		animation.setDirection(int(not animation.direction()))  # Invert animation direction
 		animation.start()  # Restart animation
 

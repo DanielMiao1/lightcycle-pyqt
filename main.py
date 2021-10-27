f24839cca9025c91b97949748a93bd3def9f31f8
Daniel M <danielmiao2019@icloud.com>
2021-10-27 10:03:43 -0700

    added main window

--- main.py
+++ main.py
@@ -5,13 +5,94 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 
+class Text(QLabel):
+	"""Override existing QLabel for color animation"""
+	def __init__(self, parent, text=""):
+		super().__init__(parent=parent)  # Initialize text
+		self.setText(text)  # Set text content
+
+	def setColor(self, color):
+		"""Color animation"""
+		palette = self.palette()  # Create new palette
+		palette.setColor(self.foregroundRole(), color)  # Set color for palette
+		self.setPalette(palette)  # Set color of text to palette color
+
+	color = pyqtProperty(QColor, fset=setColor)  # Define new PyQt Property for animation
+
+
+class Button(QPushButton):
+	"""Override existsing QPushButton for custom hover events"""
+	def __init__(self, parent, text=""):
+		super().__init__(parent=parent)  # Initialize button
+		self.setText(text)  # Set text content
+		self.setStyleSheet("background-color: white; border: none;")  # Set QSS stylesheet attributes
+
+	def enterEvent(self, event):
+		"""Mouse hover in event"""
+		super().enterEvent(event)
+
+	def leaveEvent(self, event):
+		"""Mouse hover out event"""
+		super().leaveEvent(event)
+
+
+class GameWindow(QWidget):
+	def __init__(self, parent):
+		super().__init__(parent=parent)  # Initialize page
+
+
+class MainWindow(QWidget):
+	def __init__(self, parent):
+		super().__init__(parent=parent)  # Initialize page
+		# Title
+		self.title = Text(self, "LightCycle")  # Add title
+		self.title.setAlignment(Qt.AlignCenter)  # Set title alignment
+		# Title animation
+		self.title_animation = QPropertyAnimation(self.title, b"color")  # Create animation
+		self.title_animation.setLoopCount(1)  # Set loop count
+		self.title_animation.setDuration(10000)  # Set loop duration
+		self.title_animation.setStartValue(QColor("#0000FF"))  # Set start color
+		self.title_animation.setEndValue(QColor("#AA00FF"))  # Set end color
+		self.title_animation.finished.connect(lambda: self.changeAnimationDirection(self.title_animation))  # Call lambda: sel... when animation finishes
+		self.title_animation.start()  # Start animation
+		# Start button
+		self.start_button = Button(self, "Start Game")
+
+	def changeAnimationDirection(self, animation):
+		animation.setDirection(int(not animation.direction()))  # Invert animation direction
+		animation.start()  # Restart animation
+
+	def resizeEvent(self, event):
+		"""Window resize event"""
+		# Title
+		self.title.setFont(QFont("Impact", event.size().width() // 12))  # Enlarge/shrink the title font size
+		self.title.resize(QSize(event.size().width(), event.size().height() // 5))  # Resize title
+		# Start button
+		self.start_button.resize(QSize(event.size().width() // 7, event.size().height() // 10))  # Resize start button according to the new window size. This statement has to be before the button move statement (on the next line)
+		self.start_button.move(QPoint((event.size().width() // 2) - (self.start_button.width() // 2), event.size().height() // 2))  # Move start button to center of screen
+		super().resizeEvent(event)
+
+
 class Window(QMainWindow):
 	def __init__(self):
-		super(Window, self).__init__()
-		button = QPushButton(self)
-		button.setText("button")
-		self.show()
+		super(Window, self).__init__()  # Initialize window
+		self.setMinimumSize(QSize(1080, 720))  # Set window minimum size
+		self.setWindowTitle("LightCycle")  # Set window title
+		self.stacked_pages = QStackedWidget(self)  # Add stacked widget
+		self.pages = {
+			"main": MainWindow(self),
+			"game": GameWindow(self)
+		}  # Add pages
+		# Add pages to stacked widget
+		self.stacked_pages.addWidget(self.pages["main"])
+		self.stacked_pages.addWidget(self.pages["game"])
+		self.stacked_pages.move(0, 0)  # Move pages
+		self.show()  # Show window
+
+	def resizeEvent(self, event):
+		"""Window resize event"""
+		self.stacked_pages.resize(event.size())
 
 
-application, window = QApplication([]), Window()
-application.exec_()
+application, window = QApplication([]), Window()  # Create new QApplication and instance of Window()
+application.exec_()  # Execute application
